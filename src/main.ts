b43a5eb425ceb15225fbe496d29e4bc3905c1e9b
Lukas Böhler <lukas@boehlerbrothers.com>
2023-10-10 19:05:17 +0200

    Static.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import './assets/main.css';
 
+import { ViteSSG } from 'vite-ssg/single-page'
+import App from './App.vue'
 import Gleap from 'gleap';
-import { createApp } from 'vue';
-import App from './App.vue';
 
-// Please make sure to call this method only once!
-Gleap.initialize("ogWhNhuiZcGWrva5nlDS8l7a78OfaLlV");
+// `export const createApp` is required instead of the original `createApp(App).mount('#app')`
+export const createApp = ViteSSG(
+    App,
+    async ({ isClient }) => {
+        if (isClient) {
+            const GleapModule = await import('gleap');
+            const Gleap: any = GleapModule.default || GleapModule;
 
-createApp(App).mount('#app')
+            Gleap.initialize("ogWhNhuiZcGWrva5nlDS8l7a78OfaLlV");
+        }
+    }
+)
