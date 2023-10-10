0792bca38ccad1e4220ffe73745ec6f01c34f012
Lukas Böhler <lukas@boehlerbrothers.com>
2023-10-10 18:40:23 +0200

    init.

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import './assets/main.css';
+
+import Gleap from 'gleap';
+import { createApp } from 'vue';
+import App from './App.vue';
+
+// Please make sure to call this method only once!
+Gleap.initialize("ogWhNhuiZcGWrva5nlDS8l7a78OfaLlV");
+
+createApp(App).mount('#app')
